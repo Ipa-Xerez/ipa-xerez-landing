ae88c3ec3b1c502188685aee1fcc18fa59bff2ef
Ipa-Xerez <ipaagrupacionxerez@gmail.com>
2026-02-24 20:17:31 +0100

    Update vite.ts

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -48,8 +48,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const root = process.cwd();
-  const distPath = path.resolve(root, "dist/public");
+  const distPath = path.resolve(__dirname, "../../dist/public");
   const indexHtml = path.resolve(distPath, "index.html");
 
   app.use(express.static(distPath));
