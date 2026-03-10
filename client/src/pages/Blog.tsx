2ff579af21aacb6b3e1d9876a5839121360b6861
Agrupacion Ipa Xerez <ipaagrupacionxerez@gmail.com>
2026-03-10 15:13:08 +0100

    Create Blog.tsx

---
+++ client/src/pages/Blog.tsx
@@ -0,0 +1,55 @@
+import { trpc } from "../utils/trpc";
+import { Link } from "react-router-dom";
+
+export default function Blog() {
+  const { data: posts, isLoading } = trpc.blog.list.useQuery();
+
+  if (isLoading) {
+    return <div style={{ padding: 40 }}>Cargando artículos...</div>;
+  }
+
+  if (!posts || posts.length === 0) {
+    return <div style={{ padding: 40 }}>No hay artículos publicados todavía.</div>;
+  }
+
+  return (
+    <div style={{ padding: 40, maxWidth: 800, margin: "0 auto" }}>
+      <h1>Blog</h1>
+
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          style={{
+            padding: 20,
+            borderBottom: "1px solid #ddd",
+            marginBottom: 20,
+          }}
+        >
+          <h2>{post.title}</h2>
+          <p>{post.excerpt}</p>
+
+          {post.image && (
+            <img
+              src={post.image}
+              alt={post.title}
+              style={{ width: "100%", marginTop: 10, borderRadius: 8 }}
+            />
+          )}
+
+          <Link
+            to={`/blog/${post.id}`}
+            style={{
+              display: "inline-block",
+              marginTop: 10,
+              color: "blue",
+              textDecoration: "underline",
+            }}
+          >
+            Leer más
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
